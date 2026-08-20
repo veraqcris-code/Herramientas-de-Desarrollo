195e3f3d671920f59ad1c0b85b8bc2219e429547
Jerson Olivares <jerson.paul.olivares.panta@gmail.com>
2026-08-20 15:58:59 -0500

    Formularios en proceso

---
+++ Formularios.js
@@ -0,0 +1,20 @@
+﻿function Formulario() {
+  return (
+    <form>
+      <h2>Formulario de contacto</h2>
+
+      <label htmlFor="nombre">Nombre</label>
+      <input type="text" id="nombre" name="nombre" placeholder="Escribe tu nombre" />
+
+      <label htmlFor="correo">Correo</label>
+      <input type="email" id="correo" name="correo" placeholder="correo@ejemplo.com" />
+
+      <label htmlFor="mensaje">Mensaje</label>
+      <textarea id="mensaje" name="mensaje" placeholder="Escribe tu mensaje" />
+
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
+
+export default Formulario;
